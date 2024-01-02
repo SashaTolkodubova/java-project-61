2f8eac2a294b8aa2a0f375eadeb23d18696dbefa
SashaTolkodubova <konstantin@KonstantinsMini.user.nevalink.net>
2024-01-02 23:55:00 +0300

    add code4

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -5,11 +5,9 @@ package hexlet.code;
 public final class Engine {
     public void play(Games game) {
         System.out.println(game.getInstruction());
-        int rounds = 3;
-        while (rounds > 0) {
+        for (int i = 0; i < 3; i++) {
             if (game.round()) {
                 System.out.println("Correct!");
-                rounds--;
             } else {
                 System.out.println(game.getAnswer() + " is wrong answer ;(. Correct answer was "
                         + game.getCorrectAnswer() + ".\n"
